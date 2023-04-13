f6504351ea1c8cd127d66525c7d7912b60caf7b7
Peter Aldhous <peter@peteraldhous.com>
2023-04-13 11:01:41 -0700

    initial commit with files

---
+++ R/get_data.R
@@ -0,0 +1,12 @@
+# load required packages
+library(dplyr)
+library(readr)
+
+# our world in data timeline
+covid_owid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv") %>%
+  filter(location == "World") %>%
+  select(location, date, new_cases, new_deaths) %>%
+  tail(-1)
+
+# write to csv
+write_csv(covid_owid, "covid_owid.csv", na = "")
